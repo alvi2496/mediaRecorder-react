f9a0ca7906c08a79d15ab842d4ed88933e845148
Alvi <alvi.utsab@gmail.com>
2021-11-30 01:10:02 -0800

    add home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./_components/Home";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
---
+++ src/_components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home(){
+    return (
+        <div>
+            <button>Video Recorder</button>
+            <br/>
+            <button>Audio Recorder</button>
+        </div>
+    )
+}
